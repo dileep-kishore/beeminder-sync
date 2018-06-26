7d0b687c68454f7ce59556b29502158e585714d4
Dileep Kishore <k.dileep1994@gmail.com>
2018-06-25 21:56:22 -0400

    Docstrings for tests and double quotes for strings

--- tests/beeminder/test_beeminder_api.py
+++ tests/beeminder/test_beeminder_api.py
@@ -14,6 +14,7 @@ class TestBeeminderApi:
     """ Tests for the beeminder api """
 
     def test_config(self, beeminder_config):
+        """ Test for correct configuration settings """
         for req_field in beeminder_config['required_fields']:
             assert req_field in beeminder_config.keys(), \
                 "Beeminder configuration missing required field"
@@ -21,9 +22,10 @@ class TestBeeminderApi:
                 "Beeminder configuration has empty value in required field"
 
     def test_credentials(self, beeminder_config):
-        base_url = beeminder_config['api']
-        username = beeminder_config['username']
-        auth_token = beeminder_config['auth_token']
+        """ Test for correct credentials in `config.json` """
+        base_url = beeminder_config["api"]
+        username = beeminder_config["username"]
+        auth_token = beeminder_config["auth_token"]
         user_url = furl(base_url)
         user_url.add(path=f"users/{username}.json")
         user_url.add(args={"auth_token": auth_token})
@@ -33,10 +35,11 @@ class TestBeeminderApi:
         assert response_data['username'] == username
 
     def test_interface(self, beeminder_config):
-        base_url = beeminder_config['api']
-        username = beeminder_config['username']
-        auth_token = beeminder_config['auth_token']
+        """ Test whether the `Beeminder` interface connects to the api """
+        base_url = beeminder_config["api"]
+        username = beeminder_config["username"]
+        auth_token = beeminder_config["auth_token"]
         Beeminder(base_url, username, auth_token)
         assert True
         with pytest.raises(requests.exceptions.HTTPError):
-            Beeminder(base_url, 'test', 'token')
+            Beeminder(base_url, "test", "token")
