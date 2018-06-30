78295d021c79b8a6b58c371b5669a09efc129037
Dileep Kishore <k.dileep1994@gmail.com>
2018-06-29 22:07:34 -0400

    Update base `__init__.py` to import `BeeSync` to main scope

--- beeminder_sync/__init__.py
+++ beeminder_sync/__init__.py
@@ -1,7 +1,10 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for Beeminder Sync."""
+"""
+    Top-level package for Beeminder Sync.
+"""
 
 __author__ = """Dileep Kishore"""
 __email__ = 'k.dileep1994@gmail.com'
 __version__ = '0.1.0'
+
+
+from.beeminder_sync import BeeSync
