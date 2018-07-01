1d9f4f7aed0b0771f538ae7afd22a7be975295a0
Dileep Kishore <k.dileep1994@gmail.com>
2018-06-30 22:11:21 -0400

    `BeeSync` class will help manage configuration (Incomplete)
    It will:
    1. read, verify and write the configuration file
    2. write files to base_dir if needed
    3. Make configuration settings available to other modules of the package

--- beeminder_sync/beeminder_sync.py
+++ beeminder_sync/beeminder_sync.py
@@ -1,3 +1,65 @@
-# -*- coding: utf-8 -*-
+"""
+    The main beeminder-sync class
+"""
 
-"""Main module."""
+import pathlib
+import os
+import click
+
+from .config import read_config, verify_config
+
+
+class BeeSync:
+    """
+        The main class for the package
+        Stores configuration information
+
+        Parameters
+        ----------
+        base_dir : pathlib.Path
+            The default path to the settings directory
+        config_path : pathlib.Path
+            The default path to the configuration file
+
+        Attributes
+        ----------
+        base_dir : pathlib.Path
+        config_path : pathlib.Path
+    """
+    base_dir = pathlib.Path(click.get_app_dir('beeminder_sync'))
+    config_path = base_dir / "config.ini"
+
+    def __init__(self, base_dir: pathlib.Path, config_path: pathlib.Path) -> None:
+        self.base_dir = base_dir
+        if not self.base_dir.is_dir():
+            os.mkdir(self.base_dir)
+            # TODO: Also need to reinitialize the other files that are supposed to be here
+        if self._verify_config(config_path):
+            raise ValueError("The configuration file provided is not valid")
+        self.config_path = config_path
+        # TODO: Check if there's a config file in base_dir
+        # If contents of base_dir/config.ini and config_path are the same no need to overwrite
+
+    def _verify_config(self, config_path: pathlib.Path) -> bool:
+        """
+            Verify whether the configuration file is valid
+
+            Paramters
+            ---------
+            config_path : pathlib.Path
+                The path to the configuration file
+
+            Returns
+            -------
+            bool
+                True if the config file passed in is valid otherwise False
+        """
+        validity = False
+        if config_path.is_file():
+            if verify_config(config_path):
+                validity = True
+        else:
+            raise FileNotFoundError(f"The configuration file: {config_path} does not exist")
+        return validity
+
+    # TODO: When given a new config.ini copy this to base_dir and make backup of old
