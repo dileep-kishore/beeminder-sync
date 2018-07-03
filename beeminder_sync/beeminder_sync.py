bd2111c820e9d3588702058ebc195008de25d9f1
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-02 22:34:29 -0400

    Create `spinner` instance inside `BeeSync` class
    The spinner instance is now private to the class and can only be changed
    through the `set_spinner` method

--- beeminder_sync/beeminder_sync.py
+++ beeminder_sync/beeminder_sync.py
@@ -5,6 +5,7 @@
 import pathlib
 import os
 import click
+from typing import Dict
 
 from halo import Halo
 
@@ -31,9 +32,10 @@ class BeeSync:
     base_dir = pathlib.Path(click.get_app_dir('beeminder_sync'))
     config_path = base_dir / "config.ini"
 
-    def __init__(self, base_dir: pathlib.Path, config_path: pathlib.Path, spinner: Halo) -> None:
+    def __init__(self, base_dir: pathlib.Path, config_path: pathlib.Path) -> None:
         self.base_dir = base_dir
-        self._spinner = spinner
+        self._spinner = Halo(text="Initializing application...", color='green', spinner="dots")
+        self._spinner.start()
         if not self.base_dir.is_dir():
             os.mkdir(self.base_dir)
             # TODO: Also need to reinitialize the other files that are supposed to be here
@@ -42,6 +44,7 @@ class BeeSync:
             raise ValueError("The configuration file provided is not valid")
         self.config_path = config_path
         self.config = read_config(self.config_path)
+        self._spinner.succeed(text="Initialization successful")
         # TODO: Check if there's a config file in base_dir
         # If contents of base_dir/config.ini and config_path are the same no need to overwrite
 
@@ -111,5 +114,26 @@ class BeeSync:
         else:
             self._spinner.fail("Incorrect section or option value entered")
             raise ValueError("Incorrect section or option value entered")
+
+    def set_spinner(self, settings: Dict[str, str]) -> Halo:
+        """
+            Update settings for spinner instance
+
+            Parameters
+            ----------
+            settings : Dict[str, str]
+                A dictionary containing the updated settings for the spinner
+
+            Returns
+            -------
+            Halo
+                Returns the spinner instance
+        """
+        for key in settings:
+            if key in ['spinner', 'text', 'color']:
+                setattr(self._spinner, key, settings[key])
+            else:
+                raise KeyError("Unsupported attribute supplied to spinner instance")
+        return self._spinner
 
     # TODO: When given a new config.ini copy this to base_dir and make backup of old
