212eb69fd4f99a868cd235bfb962d4ef75b8abbc
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-08 21:44:42 -0400

    Fixed test for `config` subcommand

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,16 +12,11 @@ from beeminder_sync import cli
 def test_cli_config():
     """ Test getting configuration value """
     runner = CliRunner()
-    result = runner.invoke(cli.cli, ['config', '-s', 'beeminder', '-o', 'api'], input='y')
-    print(result.output)
+    result = runner.invoke(cli.cli, ['config', '-s', 'beeminder', '-o', 'api'], input='y', obj={})
     assert result.exit_code == 0
     assert "Initialization successful" in result.output
     assert "beeminder.api" in result.output
-
-
-def test_content():
-    """Sample pytest test function with the pytest fixture as an argument."""
-    pass
+    assert "https://www.beeminder.com/api/v1" in result.output
 
 
 def test_main_cli():
@@ -30,6 +25,6 @@ def test_main_cli():
     result = runner.invoke(cli.cli)
     assert result.exit_code == 0
     assert "Main entry point to beeminder_sync" in result.output
-    help_result = runner.invoke(cli.main, ["--help"])
+    help_result = runner.invoke(cli.cli, ["--help"])
     assert help_result.exit_code == 0
     assert "Show this message and exit." in help_result.output
