fb0c8751db5db07d8a42d465af4c4e226ccb9069
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-08 21:28:29 -0400

    Tests for the `BeeSync` class
    1. `test_init` - Tests initialization of the class instance
    2. `test_update_get` - Tests `update` and `get` methods of the instance

---
+++ tests/test_beeminder_sync.py
@@ -0,0 +1,28 @@
+"""
+    Tests for the BeeSync class
+"""
+
+
+import pytest
+
+from beeminder_sync import BeeSync
+
+
+@pytest.mark.usefixtures("config_paths")
+class TestBeeminderSync:
+    """ Tests for the BeeSync class """
+
+    def test_init(self, config_paths):
+        bee_sync = BeeSync(*config_paths)
+        assert bee_sync.base_dir == config_paths[0]
+        assert bee_sync.config_path == config_paths[1]
+        assert "beeminder" in bee_sync.config.sections()
+        options = bee_sync.config.options("beeminder")
+        assert all(x in options for x in ["api", "username", "auth_token"])
+
+    def test_update_get(self, config_paths):
+        bee_sync = BeeSync(*config_paths)
+        bee_sync.update("beeminder", "username", "tester")
+        assert bee_sync.get("beeminder", "username") == "tester"
+        bee_sync.update("beeminder", "auth_token", "token")
+        assert bee_sync.get("beeminder", "auth_token") == "token"
