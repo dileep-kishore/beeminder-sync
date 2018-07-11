6dd6e0207636e11889bb3a46a404c72ea7dbf7e9
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-10 22:22:08 -0400

    Change `StreamHandler` to `FileHandler`
    Using `BASE_DIR` to determine the log file location

--- beeminder_sync/logger.py
+++ beeminder_sync/logger.py
@@ -4,9 +4,17 @@
 
 
 import logging
+import os
 
-log = logging.getLogger()
-handler = logging.StreamHandler()
+from . import BEESYNC_DIR
+
+# TODO: Make this more modular (just importable) Maybe put this in beeminder_sync
+ENV_PATH = os.environ.get('BEESYNC_DIR')
+BASE_DIR = ENV_PATH if ENV_PATH else str(BEESYNC_DIR)
+
+
+log = logging.getLogger('beeminder_sync')
+handler = logging.FileHandler(BASE_DIR)
 formatter = logging.Formatter(
     '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 )
