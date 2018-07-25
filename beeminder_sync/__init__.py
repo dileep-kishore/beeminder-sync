1ec3acfb8b10cbde7deec3c40e21e07fc4bab2ba
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-24 21:31:03 -0400

    Automatically created `BASE_DIR` if it doesn't exist
    This behavior is needed to ensure that config folder exists for logging
    and during testing

--- beeminder_sync/__init__.py
+++ beeminder_sync/__init__.py
@@ -17,3 +17,5 @@ from.beeminder_sync import BeeSync
 BEESYNC_DIR = pathlib.Path(click.get_app_dir('beeminder_sync'))
 ENV_PATH = os.environ.get('BEESYNC_DIR')
 BASE_DIR = pathlib.Path(ENV_PATH) if ENV_PATH else BEESYNC_DIR
+if not BASE_DIR.exists():
+    BASE_DIR.mkdir()
