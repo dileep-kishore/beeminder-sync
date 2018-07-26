941172ceec273d20cbe236acf3bbdf767b745464
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-25 21:03:00 -0400

    Turn off `BeeSync` spinners during tests

--- tests/test_beeminder_sync.py
+++ tests/test_beeminder_sync.py
@@ -13,7 +13,7 @@ class TestBeeminderSync:
     """ Tests for the BeeSync class """
 
     def test_init(self, config_paths):
-        bee_sync = BeeSync(*config_paths)
+        bee_sync = BeeSync(*config_paths, spinner=False)
         assert bee_sync.base_dir == config_paths[0]
         assert bee_sync.config_path == config_paths[1]
         assert "beeminder" in bee_sync.config.sections()
@@ -21,7 +21,7 @@ class TestBeeminderSync:
         assert all(x in options for x in ["api", "username", "auth_token"])
 
     def test_update_get(self, config_paths):
-        bee_sync = BeeSync(*config_paths)
+        bee_sync = BeeSync(*config_paths, spinner=False)
         bee_sync.update("beeminder", "username", "tester")
         assert bee_sync.get("beeminder", "username") == "tester"
         bee_sync.update("beeminder", "auth_token", "token")
