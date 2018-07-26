d99343bbc21547cfcb1416b8206392123a0febe6
Dileep Kishore <k.dileep1994@gmail.com>
2018-07-25 21:29:05 -0400

    Create `BASE_DIR` even if parent directory does not exist

--- beeminder_sync/__init__.py
+++ beeminder_sync/__init__.py
@@ -18,4 +18,4 @@ BEESYNC_DIR = pathlib.Path(click.get_app_dir('beeminder_sync'))
 ENV_PATH = os.environ.get('BEESYNC_DIR')
 BASE_DIR = pathlib.Path(ENV_PATH) if ENV_PATH else BEESYNC_DIR
 if not BASE_DIR.exists():
-    BASE_DIR.mkdir()
+    BASE_DIR.mkdir(parents=True)
