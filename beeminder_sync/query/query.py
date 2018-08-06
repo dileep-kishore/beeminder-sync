3eafdb7fbfcd743e15e1dfc900e46163c406e980
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-05 21:39:19 -0400

    Initial commit for the 'query' module

---
+++ beeminder_sync/query/query.py
@@ -0,0 +1,8 @@
+"""
+    Module that contains methods to help query API JSON dumps
+"""
+
+# TODO: This should take
+# 1. response dict/json # 2. query_string
+# and return
+# subset of the dictionary
