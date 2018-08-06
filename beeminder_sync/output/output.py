3e53343f22a224f8c3be9bda8a1356bd2ccafdc0
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-05 21:39:39 -0400

    Initial commit for the 'output' module

---
+++ beeminder_sync/output/output.py
@@ -0,0 +1,8 @@
+"""
+    Module that contains functions to format API JSON dumps
+"""
+
+# TODO: This should take
+# 1. response dict/json # 2. output_format
+# and return/print
+# output in table format
