76c7236e57431932f2ef53f86fc1895e1be8f1a1
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-10 21:44:38 -0400

    Implement query method
    1. Uses `pyjq` and depends on `jq` (potential installation problems)
    2. Query string has to be `jq` compatible
    3. Returns `OrderedDict`

--- beeminder_sync/query/query.py
+++ beeminder_sync/query/query.py
@@ -1,8 +1,31 @@
 """
-    Module that contains methods to help query API JSON dumps
+    Module that contains functions to help query API JSON dumps
 """
 
-# TODO: This should take
-# 1. response dict/json # 2. query_string
-# and return
-# subset of the dictionary
+import pyjq
+
+
+def query(response: dict, query_string: str) -> dict:
+    """
+        Get subset of response dictionary using the query string
+        The format for the 'query_string' is identical to `jq`
+
+        Parameters
+        ----------
+        response : dict
+            The output of the beeminder subcommand
+        query_string : str
+            String to query the response
+            Eg. '.datapoints[] | {comment, timestamp}'
+
+        Returns
+        -------
+        dict
+            The parsed dictionary
+
+        Refer
+        -----
+        `jq` is a lightweight and flexible command-line JSON processor
+        refer: https://stedolan.github.io/jq/
+    """
+    return pyjq.all(query_string, response)
