57a89e8355d5428c1494b081181edeb16dbeeea4
Dileep Kishore <k.dileep1994@gmail.com>
2018-08-14 21:36:29 -0400

    Implement `output` function
    1. The `json_output` function works
    2. The `table_output` function hasn't been implemented yet

--- beeminder_sync/output/output.py
+++ beeminder_sync/output/output.py
@@ -2,7 +2,59 @@
     Module that contains functions to format API JSON dumps
 """
 
-# TODO: This should take
-# 1. response dict/json # 2. output_format
-# and return/print
-# output in table format
+import json
+
+
+def json_output(response: dict) -> str:
+    """
+        Output API response in prettified JSON format
+
+        Parameters
+        ----------
+        response : dict
+
+        Returns
+        -------
+        str
+    """
+    return json.dumps(response, indent=2, sort_keys=True)
+
+
+def table_output(response: dict) -> str:
+    """
+        Output API response in prettified Table format
+
+        Parameters
+        ----------
+        response : dict
+
+        Returns
+        -------
+        str
+    """
+    # TODO: How to find screen width
+    raise NotImplementedError
+
+
+def output(response: dict, format: str = "json") -> str:
+    """
+        Output API response in requested format
+
+        Parameters
+        ----------
+        response : dict
+            The API response JSON
+        format : {'json', 'table'}, optional
+            The format to convert the API response into
+            Default value is 'json'
+
+        Returns
+        -------
+        str
+            The API response in the requested format
+    """
+    if format == "json":
+        return json_output(response)
+    if format == "table":
+        return table_output(response)
+    raise ValueError("Supported formats are 'json' and 'table'")
