f1efc24133b74616263b96cad0f12609717745a7
Dileep Kishore <k.dileep1994@gmail.com>
2018-09-09 11:09:29 -0400

    Implement 'table' output option. Fixes #21.

--- beeminder_sync/output/output.py
+++ beeminder_sync/output/output.py
@@ -33,7 +33,28 @@ def table_output(response: dict) -> str:
         str
     """
     # TODO: How to find screen width
-    raise NotImplementedError
+    width = 120
+    # NOTE: We assume that results are a flat list of dictionaries inside an object
+    header = list(response.keys())[0]
+    rows = list(response[header])
+    # NOTE: We assume that all items in list have the same number of keys
+    cols = list(rows[0].keys())
+    table = ['-' * width]
+    table.append("|{content:^{width}}|".format(content=header, width=width - 2))
+    table.append('-' * width)
+    col_width = width // len(cols)
+    col_headers = []
+    for col in cols:
+        col_headers.append("|{content:^{width}}|".format(content=col, width=col_width - 2))
+    table.append(''.join(col_headers))
+    table.append('-' * width)
+    for row in rows:
+        row_data = [''] * len(row)
+        for key, value in row.items():
+            ind = cols.index(key)
+            row_data[ind] = "|{content:^{width}}|".format(content=value, width=col_width - 2)
+        table.append(''.join(row_data))
+    return '\n'.join(table)
 
 
 def output(response: dict, format: str = "json") -> str:
