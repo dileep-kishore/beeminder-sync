519870d66dee933d583be448b13b2ab721d8ca0c
Dileep Kishore <k.dileep1994@gmail.com>
2018-09-16 21:40:32 -0400

    Update tests for query after api change

--- tests/query/test_query.py
+++ tests/query/test_query.py
@@ -13,9 +13,9 @@ class TestQuery:
     def test_query(self, github_json):
         query_string1 = ".[0] | {message: .commit.message, name: .commit.committer.name}"
         data1 = query(github_json, query_string1)
-        assert isinstance(data1, list)
+        assert isinstance(data1[''], list)
         assert len(data1) == 1
         query_string2 = ".[] | {message: .commit.message, name: .commit.committer.name}"
         data2 = query(github_json, query_string2)
-        assert isinstance(data2, list)
-        assert len(data2) > 1
+        assert isinstance(data2[''], list)
+        assert len(data2['']) > 1
